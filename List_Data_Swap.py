8e8b619d7ae39efacc4d1590093017ee6eff954f
Muthuvel <muthuvl07@gmail.com>
2024-11-07 19:33:30 +0530

    Data Swap in Lists
    Swapping or replacing new values in the List of datasets for beginners

---
+++ List_Data_Swap.py
@@ -0,0 +1,6 @@
+#Below is the Defined Data List
+#We can replace the list by assigning new values
+#Values enclosed in [] changes as 0- 1st value, 1-2nd Value and so on
+list = ['b','c',92]
+list[1] = 'chuck'
+print(list)
